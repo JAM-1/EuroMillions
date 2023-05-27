f9ce75957cf50639c72d7e340ee96abbf79d6c49
JAM-1 <john.mck@sky.com>
2023-05-27 22:20:16 +0100

    Working_Version_1

---
+++ Lottery/Form1.Designer.cs
@@ -0,0 +1,251 @@
+﻿
+namespace EuroMillions
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.textBox5 = new System.Windows.Forms.TextBox();
+            this.textBox7 = new System.Windows.Forms.TextBox();
+            this.textBox8 = new System.Windows.Forms.TextBox();
+            this.btnCheckNumbers = new System.Windows.Forms.Button();
+            this.lblNumbers = new System.Windows.Forms.Label();
+            this.lblHotPicks = new System.Windows.Forms.Label();
+            this.errorProvider1 = new System.Windows.Forms.ErrorProvider(this.components);
+            this.labelEuroMillions = new System.Windows.Forms.Label();
+            this.btnClearNumbers = new System.Windows.Forms.Button();
+            this.btnGenDraws = new System.Windows.Forms.Button();
+            this.textBox6 = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(40, 99);
+            this.textBox1.MaxLength = 2;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(39, 23);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox1.Validating += new System.ComponentModel.CancelEventHandler(this.textBoxValidatingEuroMillionsNumbers);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(85, 99);
+            this.textBox2.MaxLength = 2;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(40, 23);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox2.Validating += new System.ComponentModel.CancelEventHandler(this.textBoxValidatingEuroMillionsNumbers);
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(131, 99);
+            this.textBox3.MaxLength = 2;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(40, 23);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox3.Validating += new System.ComponentModel.CancelEventHandler(this.textBoxValidatingEuroMillionsNumbers);
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(177, 99);
+            this.textBox4.MaxLength = 2;
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(40, 23);
+            this.textBox4.TabIndex = 3;
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox4.Validating += new System.ComponentModel.CancelEventHandler(this.textBoxValidatingEuroMillionsNumbers);
+            // 
+            // textBox5
+            // 
+            this.textBox5.Location = new System.Drawing.Point(223, 99);
+            this.textBox5.MaxLength = 2;
+            this.textBox5.Name = "textBox5";
+            this.textBox5.Size = new System.Drawing.Size(40, 23);
+            this.textBox5.TabIndex = 4;
+            this.textBox5.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox5.Validating += new System.ComponentModel.CancelEventHandler(this.textBoxValidatingEuroMillionsNumbers);
+            // 
+            // textBox7
+            // 
+            this.textBox7.Location = new System.Drawing.Point(282, 99);
+            this.textBox7.MaxLength = 2;
+            this.textBox7.Name = "textBox7";
+            this.textBox7.Size = new System.Drawing.Size(40, 23);
+            this.textBox7.TabIndex = 6;
+            this.textBox7.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox7.Validating += new System.ComponentModel.CancelEventHandler(this.textBoxValidatingEuroMillionsHotPicks);
+            // 
+            // textBox8
+            // 
+            this.textBox8.Location = new System.Drawing.Point(328, 99);
+            this.textBox8.MaxLength = 2;
+            this.textBox8.Name = "textBox8";
+            this.textBox8.Size = new System.Drawing.Size(40, 23);
+            this.textBox8.TabIndex = 7;
+            this.textBox8.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            this.textBox8.Validating += new System.ComponentModel.CancelEventHandler(this.textBoxValidatingEuroMillionsHotPicks);
+            // 
+            // btnCheckNumbers
+            // 
+            this.btnCheckNumbers.FlatAppearance.BorderColor = System.Drawing.Color.Blue;
+            this.btnCheckNumbers.FlatAppearance.MouseDownBackColor = System.Drawing.Color.Red;
+            this.btnCheckNumbers.FlatAppearance.MouseOverBackColor = System.Drawing.Color.White;
+            this.btnCheckNumbers.Location = new System.Drawing.Point(397, 70);
+            this.btnCheckNumbers.Name = "btnCheckNumbers";
+            this.btnCheckNumbers.Size = new System.Drawing.Size(124, 52);
+            this.btnCheckNumbers.TabIndex = 8;
+            this.btnCheckNumbers.Text = "Check EuroMillions Numbers";
+            this.btnCheckNumbers.UseVisualStyleBackColor = true;
+            this.btnCheckNumbers.Click += new System.EventHandler(this.btnCheckEuroMillionsNumbers_Click);
+            // 
+            // lblNumbers
+            // 
+            this.lblNumbers.AutoSize = true;
+            this.lblNumbers.Location = new System.Drawing.Point(93, 70);
+            this.lblNumbers.Name = "lblNumbers";
+            this.lblNumbers.Size = new System.Drawing.Size(110, 15);
+            this.lblNumbers.TabIndex = 9;
+            this.lblNumbers.Text = "Pick Numbers 1 -50";
+            // 
+            // lblHotPicks
+            // 
+            this.lblHotPicks.AutoSize = true;
+            this.lblHotPicks.Location = new System.Drawing.Point(282, 70);
+            this.lblHotPicks.Name = "lblHotPicks";
+            this.lblHotPicks.Size = new System.Drawing.Size(83, 15);
+            this.lblHotPicks.TabIndex = 10;
+            this.lblHotPicks.Text = "HotPicks 1 -12";
+            // 
+            // errorProvider1
+            // 
+            this.errorProvider1.ContainerControl = this;
+            // 
+            // labelEuroMillions
+            // 
+            this.labelEuroMillions.AutoSize = true;
+            this.labelEuroMillions.Location = new System.Drawing.Point(165, 35);
+            this.labelEuroMillions.Name = "labelEuroMillions";
+            this.labelEuroMillions.Size = new System.Drawing.Size(73, 15);
+            this.labelEuroMillions.TabIndex = 11;
+            this.labelEuroMillions.Text = "EuroMillions";
+            // 
+            // btnClearNumbers
+            // 
+            this.btnClearNumbers.Location = new System.Drawing.Point(165, 128);
+            this.btnClearNumbers.Name = "btnClearNumbers";
+            this.btnClearNumbers.Size = new System.Drawing.Size(122, 23);
+            this.btnClearNumbers.TabIndex = 12;
+            this.btnClearNumbers.Text = "Clear Numbers";
+            this.btnClearNumbers.UseVisualStyleBackColor = true;
+            this.btnClearNumbers.Click += new System.EventHandler(this.btnClearNumbers_Click);
+            // 
+            // btnGenDraws
+            // 
+            this.btnGenDraws.Location = new System.Drawing.Point(397, 226);
+            this.btnGenDraws.Name = "btnGenDraws";
+            this.btnGenDraws.Size = new System.Drawing.Size(124, 55);
+            this.btnGenDraws.TabIndex = 13;
+            this.btnGenDraws.Text = "Generate Draws";
+            this.btnGenDraws.UseMnemonic = false;
+            this.btnGenDraws.UseVisualStyleBackColor = true;
+            this.btnGenDraws.Click += new System.EventHandler(this.btnGenerateDraws_Click);
+            // 
+            // textBox6
+            // 
+            this.textBox6.Location = new System.Drawing.Point(114, 226);
+            this.textBox6.Name = "textBox6";
+            this.textBox6.Size = new System.Drawing.Size(173, 23);
+            this.textBox6.TabIndex = 14;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(156, 196);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(95, 15);
+            this.label1.TabIndex = 15;
+            this.label1.Text = "Number of Years";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(563, 460);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBox6);
+            this.Controls.Add(this.btnGenDraws);
+            this.Controls.Add(this.btnClearNumbers);
+            this.Controls.Add(this.labelEuroMillions);
+            this.Controls.Add(this.lblHotPicks);
+            this.Controls.Add(this.lblNumbers);
+            this.Controls.Add(this.btnCheckNumbers);
+            this.Controls.Add(this.textBox8);
+            this.Controls.Add(this.textBox7);
+            this.Controls.Add(this.textBox5);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Name = "Form1";
+            this.RightToLeftLayout = true;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.errorProvider1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.TextBox textBox5;
+        private System.Windows.Forms.TextBox textBox7;
+        private System.Windows.Forms.TextBox textBox8;
+        private System.Windows.Forms.Button btnCheckNumbers;
+        private System.Windows.Forms.Label lblNumbers;
+        private System.Windows.Forms.Label lblHotPicks;
+        private System.Windows.Forms.ErrorProvider errorProvider1;
+        private System.Windows.Forms.Label labelEuroMillions;
+        private System.Windows.Forms.Button btnClearNumbers;
+        private System.Windows.Forms.Button btnGenDraws;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox6;
+    }
+}
+
---
+++ Lottery/Form1.cs
@@ -0,0 +1,402 @@
+﻿using HtmlAgilityPack;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text.RegularExpressions;
+using System.Windows.Forms;
+
+namespace EuroMillions
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+			this.Text = "EuroMillions";
+		}
+
+		#region EuroMillions2
+
+		private void btnCheckEuroMillionsNumbers_Click(object sender, EventArgs e)
+        {
+			CompareEuroMillionsInputs(GetEuroMillionsNumbers(), GetEuroMillionsResults(GetListOfEuroMillionYears()));
+        }
+
+		// Get User Input
+		private List<string> GetEuroMillionsNumbers()
+		{
+			List<string> userNumbers = new List<string>();
+			List<string> userHotPicks = new List<string>();
+
+			if (textBox1.Text != null 
+				|| textBox2.Text != null
+				|| textBox3.Text != null
+				|| textBox4.Text != null
+				|| textBox5.Text != null
+				|| textBox7.Text != null
+				|| textBox8.Text != null
+				)
+			{
+				string numberOne = textBox1.Text;
+				userNumbers.Add(DoubleDigit(numberOne));
+				string numberTwo = textBox2.Text;
+				userNumbers.Add(DoubleDigit(numberTwo));
+				string numberThree = textBox3.Text;
+				userNumbers.Add(DoubleDigit(numberThree));
+				string numberFour = textBox4.Text;
+				userNumbers.Add(DoubleDigit(numberFour));
+				string numberFive = textBox5.Text;
+				userNumbers.Add(DoubleDigit(numberFive));
+				string hotpickOne = textBox7.Text;
+				userHotPicks.Add(DoubleDigit(hotpickOne));
+				string hotpickTwo = textBox8.Text;
+				userHotPicks.Add(DoubleDigit(hotpickTwo));
+
+
+				userNumbers.Sort();
+				userHotPicks.Sort(); // wrong way round??
+
+				foreach (string hotpick in userHotPicks)
+                {
+					userNumbers.Add(hotpick);
+                }
+			}
+
+			return userNumbers;
+		}
+
+		// Get List of Years EuroMillions has been active
+		private static List<string> GetListOfEuroMillionYears()
+		{
+			List<string> euroMillionYearsActive = new List<string>();
+
+			HtmlWeb web = new HtmlWeb();
+			HtmlAgilityPack.HtmlDocument doc = web.Load($"https://www.lottery.co.uk/euromillions/results/archive-2004");
+
+			HtmlNodeCollection yearsActive = doc.DocumentNode.SelectNodes("//a[contains(@title,'EuroMillions results')]");
+
+			foreach (HtmlNode year in yearsActive)
+			{
+				euroMillionYearsActive.Add(year.InnerText.ToString());
+			}
+
+			return euroMillionYearsActive;
+		}
+
+		// Get Results for every draw for the year given
+		private static List<List<string>> GetEuroMillionsResultsPerYear(string euroMillionsSingleYear)
+		{
+			List<string> singleResult = new List<string>();
+			List<List<string>> yearlyResults = new List<List<string>>();
+
+			HtmlWeb web = new HtmlWeb();
+			HtmlAgilityPack.HtmlDocument doc = web.Load($"https://www.lottery.co.uk/euromillions/results/archive-{euroMillionsSingleYear}");
+
+			HtmlNodeCollection resultsDate = doc.DocumentNode.SelectNodes("//tr/td[1]");
+			HtmlNodeCollection resultsNumbers = doc.DocumentNode.SelectNodes("//tr/td[2]");
+
+			int i = 0;
+			foreach (var date in resultsDate)
+			{
+				int ii = 0;
+				singleResult.Add(date.InnerText);
+				string[] cleanNumbers = Regex.Split(resultsNumbers[i].InnerText, @"(\d+)");
+
+				foreach (string number in cleanNumbers)
+				{
+
+					if (ii % 2 != 0)
+					{
+						singleResult.Add(DoubleDigit(number));
+					}
+					ii++;
+				}
+				i++;
+
+				// Add yearly results to File
+				System.IO.File.AppendAllLines(@"C:\Users\Public\TestFolder\EuroMillions.txt", singleResult);
+
+				yearlyResults.Add(singleResult);
+				singleResult = new List<string>();
+			}
+
+			return yearlyResults;
+		}
+
+		// Get Results for every draw for every year active
+		private static List<List<List<string>>> GetEuroMillionsResults(List<string> euroMillionsYearsActive)
+		{
+			List<List<List<string>>> completeEuroMillionsResults = new List<List<List<string>>>();
+			System.IO.File.WriteAllText(@"C:\Users\Public\TestFolder\EuroMillions.txt", string.Empty);
+
+			foreach (string year in euroMillionsYearsActive)
+			{
+				completeEuroMillionsResults.Add(GetEuroMillionsResultsPerYear(year));
+			}
+
+			return completeEuroMillionsResults;
+		}
+
+		// Compare Inputs
+		private bool CompareEuroMillionsInputs(List<string> userNumbers, List<List<List<string>>> completeEuroMillionsResults)
+        {
+			string closestMatch = null;
+			int highestNumber = 0;
+
+			foreach (List<List<string>> yearResults in completeEuroMillionsResults)
+            {
+				foreach (List<string> individualDraw in yearResults)
+                {
+					int numberFound = 0;
+					int hotPickFound = 0;
+
+					for (int ii = 5; ii < userNumbers.Count + 1; ii++)
+					{
+						if (individualDraw[ii].Equals(userNumbers[ii - 1]))
+						{
+							hotPickFound++;
+						}
+					}
+
+					for (int i = 1; i < userNumbers.Count-1; i++)
+                    {
+						
+                        if (individualDraw[i].Equals(userNumbers[i - 1]))
+						{
+							numberFound++;
+
+							if (numberFound > highestNumber)
+							{
+								highestNumber = numberFound;
+								if (hotPickFound > 0)
+								{
+									closestMatch = $"The closest you came to a win was on {individualDraw[0]} with {highestNumber} matching numbers and {hotPickFound} hotpick(s)";
+								}
+                                else
+                                {
+									closestMatch = $"The closest you came to a win was on {individualDraw[0]} with {highestNumber} matching numbers";
+								}
+							}
+
+							if (numberFound == 7)
+                            {
+								System.IO.File.WriteAllText(@"C:\Users\Public\TestFolder\EuroMillionsResult.txt", individualDraw[0].ToString());
+								MessageBox.Show($"These numbers won the EuroMillions on {individualDraw[0]}");
+								return true;
+							}
+						}
+                    }
+
+					hotPickFound = 0;
+					numberFound = 0;
+				}
+            }
+
+			MessageBox.Show($"These numbers have never won the EuroMillions. {closestMatch}");
+			return false;
+		}
+
+		// Validate the User Input Numbers
+		private void textBoxValidatingEuroMillionsNumbers(object sender, CancelEventArgs e)
+		{	
+			List<string> selectedNumbers = new List<string>();
+
+			TextBox tbValue = sender as TextBox;
+			if (string.IsNullOrWhiteSpace(tbValue.Text) || Int32.Parse(tbValue.Text) < 1 || Int32.Parse(tbValue.Text) > 50 || selectedNumbers.Contains(tbValue.Text))
+			{
+				e.Cancel = true;
+				textBox1.Focus();
+				errorProvider1.SetError(textBox1, "");
+				MessageBox.Show("Number box cannot be left blank, less than 1 or greater than 50 or previously entered");
+			}
+			else
+			{
+				selectedNumbers.Add(tbValue.Text);
+				e.Cancel = false;
+				errorProvider1.SetError(textBox1, "");
+			}
+		}
+
+		// Validate the USer Input HotPicks
+		private void textBoxValidatingEuroMillionsHotPicks(object sender, CancelEventArgs e)
+		{
+			TextBox tbValue = sender as TextBox;
+			if (string.IsNullOrWhiteSpace(tbValue.Text) || Int32.Parse(tbValue.Text) < 1 || Int32.Parse(tbValue.Text) > 12)
+			{
+				e.Cancel = true;
+				textBox1.Focus();
+				errorProvider1.SetError(textBox1, "");
+				MessageBox.Show("HotPick box cannot be left blank, less than 1 or greater than 12");
+			}
+			else
+			{
+				e.Cancel = false;
+				errorProvider1.SetError(textBox1, "");
+			}
+		}
+
+		// Pad any single digit number (User input or Draw numbers) to a double digit
+		private static string DoubleDigit(string number)
+		{
+			string newNumber = null;
+
+			if (number.Length < 2)
+			{
+				newNumber = number.PadLeft(2, '0');
+			}
+			else
+			{
+				newNumber = number;
+			}
+
+			return newNumber;
+		}
+
+		// Clear all Text Boxes
+        private void btnClearNumbers_Click(object sender, EventArgs e)
+        {
+			Action<Control.ControlCollection> func = null;
+
+			func = (controls) =>
+			{
+				foreach (Control control in controls)
+					if (control is TextBox)
+						(control as TextBox).Clear();
+					else
+						func(control.Controls);
+			};
+
+			func(Controls);
+		}
+
+		private void btnGenerateDraws_Click(object sender, EventArgs e)
+		{
+			Random r = new Random();
+			if (textBox6.Text != null)
+			{
+				// SetUp - create Dictionaries
+				// Create Dictionary for Numbers drawn
+				Dictionary<int, int> genNumbersDictionary = new Dictionary<int, int>();
+				{
+					for (int i = 1; i < 51; i++)
+					{
+						genNumbersDictionary.Add(i, 0);
+					}
+				}
+
+				// Create Dictionary for BonusBall drawn
+				Dictionary<int, int> genBonusballDictionary = new Dictionary<int, int>();
+				{
+					for (int i = 1; i < 13; i++)
+					{
+						genBonusballDictionary.Add(i, 0);
+					}
+				}
+
+				int drawnNumberIndex;
+				int drawnHotPickIndex;
+				int drawnNumber;
+				int drawnHotPick;
+
+				long numberofDraws = long.Parse(textBox6.Text);
+				// Gen Draws using TextBox.7 Int
+				for (int i = 1; i < (numberofDraws * 12 * 4); i++)
+				{
+
+					List<int> avaliableNumbers = new List<int>();
+					{
+						for (int ii = 1; ii < 51; ii++)
+						{
+							avaliableNumbers.Add(ii);
+						}
+					}
+
+					List<int> avaliableHotPicks = new List<int>();
+					{
+						for (int ii = 1; ii < 13; ii++)
+						{
+							avaliableHotPicks.Add(ii);
+						}
+					}
+
+					for (int draw = 1; draw <= 5; draw++)
+					{
+						//RandomNumberGenerator a = new RandomNumberGenerator;
+						drawnNumberIndex = r.Next(avaliableNumbers.Count);
+						drawnNumber = avaliableNumbers[drawnNumberIndex];
+						genNumbersDictionary[drawnNumber] += 1;
+						int lastNumber = avaliableNumbers[avaliableNumbers.Count - 1];
+						avaliableNumbers.Insert(drawnNumberIndex, lastNumber);
+						avaliableNumbers.RemoveAt(drawnNumberIndex + 1);
+						avaliableNumbers.RemoveAt(avaliableNumbers.Count - 1);
+						//avaliableNumbers = avaliableNumbers;
+					}
+
+					for (int hotpick = 0; hotpick < 2; hotpick++)
+					{
+						//Random r = new Random();
+						drawnHotPickIndex = r.Next(avaliableHotPicks.Count);
+						drawnHotPick = avaliableHotPicks[drawnHotPickIndex];
+						genBonusballDictionary[drawnHotPick] += 1;
+					}
+				}
+
+				//var sortedNumbers = from entry in genNumbersDictionary orderby entry.Value ascending select entry;
+				//var sortedBonusBall = from entry in genBonusballDictionary orderby entry.Value ascending select entry;
+
+				var sortedNumbersOne = genNumbersDictionary.OrderBy(x => x.Value);
+				Dictionary<int, int> sortedNumbers = sortedNumbersOne.ToDictionary(t => t.Key, t => t.Value);
+
+				var sortedBonusBallOne = genBonusballDictionary.OrderBy(x => x.Value);
+				Dictionary<int, int> sortedBonusBall = sortedBonusBallOne.ToDictionary(t => t.Key, t => t.Value);
+
+				List<string> genDraw = new List<string>();
+				List<string> genHotPicks = new List<string>();
+				for (int iii = 0; iii < 5; iii++)
+                {
+					genDraw.Add(sortedNumbers.Keys.ElementAt(iii).ToString());
+					genDraw.Sort();
+                }
+				genDraw.Sort();
+				for (int iv = 0; iv < 2; iv++)
+                {
+					genHotPicks.Add(sortedBonusBall.Keys.ElementAt(iv).ToString());
+                }
+				genHotPicks.Sort();
+				foreach (string hotpick in genHotPicks)
+                {
+					genDraw.Add(hotpick);
+                }
+
+				if (CompareEuroMillionsInputs(genDraw, GetEuroMillionsResults(GetListOfEuroMillionYears())))
+                {
+					MessageBox.Show($"These numbers have been drawn the least amount of time in {(numberofDraws * 12 * 4)} Draws. " +
+					$"{sortedNumbers.Keys.ElementAt(0)}," +
+					$"{sortedNumbers.Keys.ElementAt(1)}," +
+					$"{sortedNumbers.Keys.ElementAt(2)}," +
+					$"{sortedNumbers.Keys.ElementAt(3)}," +
+					$"{sortedNumbers.Keys.ElementAt(4)}");
+					MessageBox.Show($"This bonusball has been drawn the least amount of time  {(numberofDraws * 12 * 4)} Draws. {sortedBonusBall.Keys.ElementAt(0)}, {sortedBonusBall.Keys.ElementAt(1)}");
+
+				}
+				else
+                {
+					MessageBox.Show($"Numbers : " +
+						$"{sortedNumbers.Keys.ElementAt(0)}," +
+						$"{sortedNumbers.Keys.ElementAt(1)}," +
+						$"{sortedNumbers.Keys.ElementAt(2)}," +
+						$"{sortedNumbers.Keys.ElementAt(3)}," +
+						$"{sortedNumbers.Keys.ElementAt(4)}," +
+						$"{sortedBonusBall.Keys.ElementAt(0)}," +
+						$"{sortedBonusBall.Keys.ElementAt(1)}"
+						);
+
+					btnGenerateDraws_Click(sender, e);
+				}
+			}
+		}
+
+        #endregion
+    }
+}
